ceb4a4380316d08d7be131428bed4575325aac31
ch-ushasree <nlm.qwerty1289@gmail.com>
2026-08-25 16:40:49 +0530

    Fix Vercel deployment

---
+++ src/main.ts
@@ -0,0 +1,25 @@
+import express, { Application } from 'express';
+import cors from 'cors';
+import { Handlers } from './handlers';
+
+const app: Application = express();
+const port = process.env.PORT || 5100;
+
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+const handlers = new Handlers();
+
+app.get('/', handlers.getRoot);
+//Get Graph
+app.get('/graph', handlers.getGraph);
+
+app.get('/data', handlers.getData);
+
+if (require.main === module) {
+    app.listen(port, (): void => {
+        console.log(`Server is Running on Port ${port}`);
+    });
+}
+
+export default app;
